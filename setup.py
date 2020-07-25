738e29131d5db8a048f2445270c0d55acfab0538
root <evarist@blackcreek.tech>
2020-07-26 07:17:54 +0800

    Modify the .ignore file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A script to Bring up a Hyperledger Fabric network",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/eirtscience/blocknet",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
